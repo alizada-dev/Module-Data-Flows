a8a7d226ab8e63993ef0944e48985bc0fcd5444f
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-04-14 20:34:13 +0100

    Added the search functionality

--- prep/script.js
+++ prep/script.js
@@ -1,37 +1,47 @@
-const films = [
-    {
-        title: "Intersteller",
-        director: "Christopher Nolan",
-        times: ["15:35"],
-        certificate: "15",
-        duration: 190
-    },
-    {
-        title: "ZNMD",
-        director: "Zoya Akhtar",
-        times: ["15:00", "20:00"],
-        certificate: "12A",
-        duration: 153
-    },
-    {
-        title: "PK",
-        director: "Rajkumar",
-        times: ["13:15", "15:15", "17:15"],
-        certificate: "8B",
-        duration: 165
-    },
-    {
-        title: "3 Idiots",
-        director: "Rajkumar Hirani",
-        times: ["13:15", "15:15", "17:15"],
-        certificate: "98C",
-        duration: 198
-    }
-]
 
-const container = document.getElementById("container");
+const state = {
+    films: [
+        {
+            title: "Intersteller",
+            director: "Christopher Nolan",
+            times: ["15:35"],
+            certificate: "15",
+            duration: 190
+        },
+        {
+            title: "ZNMD",
+            director: "Zoya Akhtar",
+            times: ["15:00", "20:00"],
+            certificate: "12A",
+            duration: 153
+        },
+        {
+            title: "PK",
+            director: "Rajkumar",
+            times: ["13:15", "15:15", "17:15"],
+            certificate: "8B",
+            duration: 165
+        },
+        {
+            title: "3 Idiots",
+            director: "Rajkumar Hirani",
+            times: ["13:15", "15:15", "17:15"],
+            certificate: "98C",
+            duration: 198
+        },
+        {
+            title: "Inception",
+            director: "Christopher Nolan",
+            times: ["13:00", "15:00"],
+            certificate: "7C",
+            duration: 128
+        }
+    ],
 
-const template = container.querySelector("#film-card");
+    searchTerm: "",
+}
+
+const template = document.getElementById("film-card");
 
 const createFilmCard = (film) => {
     const card = template.content.cloneNode(true);
@@ -49,6 +59,33 @@ const createFilmCard = (film) => {
     return card;
 }
 
-const filmCard = films.map(createFilmCard);
+const container = document.getElementById("container");
 
-container.append(...filmCard);
+function render() {
+    container.textContent = "";
+
+    const filteredFilms = state.films.filter((film) => {
+
+        const term = state.searchTerm.toLowerCase();
+        
+        return film.title.toLowerCase().includes(term) 
+    });
+
+
+    const filmCard = filteredFilms.map(createFilmCard);
+
+
+    container.append(...filmCard);
+}
+
+
+const searchBox = document.getElementById("search");
+
+searchBox.addEventListener("input", handleInput);
+
+function handleInput(event) {
+    state.searchTerm = event.target.value;
+    render();
+}
+
+render();
