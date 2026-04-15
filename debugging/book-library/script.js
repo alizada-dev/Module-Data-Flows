d99a1abdf09f22a760cce343f5fe155bcbb6e996
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-04-15 11:30:37 +0100

    variable names changed

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -19,37 +19,37 @@ function populateStorage() {
   }
 }
 
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const check = document.getElementById("check");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readCheckbox = document.getElementById("check");
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
   if (
-    !title.value.trim() ||
-    !author.value.trim() ||
-    !pages.value.trim()
+    !titleInput.value.trim() ||
+    !authorInput.value.trim() ||
+    !pagesInput.value.trim()
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
+    let book = new Book(titleInput.value, authorInput.value, pagesInput.value, readCheckbox.checked);
     myLibrary.push(book);
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    check.checked = false;
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readCheckbox.checked = false;
     render();
   }
 }
 
 function Book(title, author, pages, check) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.check = check;
+  this.titleInput = title;
+  this.authorInput = author;
+  this.pagesInput = pages;
+  this.readCheckbox = check;
 }
 
 function render() {
@@ -68,9 +68,9 @@ function render() {
     const pagesCell = row.insertCell(2);
     const wasReadCell = row.insertCell(3);
     const deleteCell = row.insertCell(4);
-    titleCell.textContent = myLibrary[i].title;
-    authorCell.textContent = myLibrary[i].author;
-    pagesCell.textContent = myLibrary[i].pages;
+    titleCell.textContent = myLibrary[i].titleInput;
+    authorCell.textContent = myLibrary[i].authorInput;
+    pagesCell.textContent = myLibrary[i].pagesInput;
 
     //add and wait for action for read/unread button
     const changeBut = document.createElement("button");
